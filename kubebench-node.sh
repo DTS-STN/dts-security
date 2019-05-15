96b4124bc8e59082d6b3c8275b12e0180c9fcc3f
Ben Larabie <benlarabie@gmail.com>
2019-05-15 13:02:04 -0400

    kube-bench-node for aks nodes

---
+++ kubebench-node.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+kubectl run --rm -i -t kube-bench-node --image=aquasec/kube-bench:latest --restart=Never --overrides="{ \"apiVersion\": \"v1\", \"spec\": { \"hostPID\": true } }" -- node --version $1
